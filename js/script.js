18935f948a9760232b46d3a02c50241dce09a930
VitorBarbosa1 <VITORBARBOSA1301@GMAIL.COM>
2023-11-26 16:25:28 +0000

    more examples added

--- js/script.js
+++ js/script.js
@@ -60,3 +60,38 @@ $('.module-nav').on('click', function ()
 	$(allModulesCardsSelector).removeClass('card-highlight');
 	$(thisModuleCardsSelector).addClass('card-highlight');
 });
+// Selecting children elements
+$('p').click(function ()
+			  {
+	              $(this).children('a').css('background-color', 'yellow');
+			  });
+
+// targetting next element
+$("button").click(function ()
+                  {
+	                  $(this).next().slideToggle('slow');
+                  });
+
+
+
+
+$('#reset').click(function ()
+                  {
+	                  $(this).siblings().children().removeClass('red').removeClass('yellow').removeClass('green')
+	                         .removeClass('large-font');
+                  });
+
+$('#card-btn-1').click(function ()
+                       {
+	                       $(this).siblings('span').addClass('yellow');
+                       });
+
+$('#card-btn-2').click(function ()
+                       {
+	                       $(this).siblings('#card-par-1').addClass('red');
+                       });
+
+$('#card-btn-3').click(function ()
+                       {
+	                       $(this).siblings().addClass('large-font');
+                       });
